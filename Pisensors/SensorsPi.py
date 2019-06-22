31619bcf31e1d616c67c9922ef8e24dcfa93095f
EricJLi <li.eric1998@gmail.com>
2019-06-22 13:18:40 -0400

    Update Distance Sensor in firestore

--- Pisensors/SensorsPi.py
+++ Pisensors/SensorsPi.py
@@ -1,53 +1,52 @@
 import RPi.GPIO as GPIO
 import time
-from firebase import firebase
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
+cred = credentials.Certificate('./serviceAccountKey.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
 x = 0
 
-firebase = firebase.FirebaseApplication('https://rts-monitor.firebaseio.com', None)
-new_user = 'Ozgur Vatansever'
-
-result = firebase.post('devices', new_user, {'print': 'pretty'}, {'X_FANCY_HEADER': 'VERY FANCY'})
-print result(
-{u'name': u'-Io26123nDHkfybDIGl7'})
-
-result = firebase.post('/users', new_user, {'print': 'silent'}, {'X_FANCY_HEADER': 'VERY FANCY'})
-print result == None
-True
-
-'''
 while x < 1:
-      GPIO.setmode(GPIO.BOARD)
+    GPIO.setmode(GPIO.BOARD)
 
-      PIN_TRIGGER = 7
-      PIN_ECHO = 11
+    PIN_TRIGGER = 7
+    PIN_ECHO = 11
 
-      GPIO.setup(PIN_TRIGGER, GPIO.OUT)
-      GPIO.setup(PIN_ECHO, GPIO.IN)
+    GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+    GPIO.setup(PIN_ECHO, GPIO.IN)
 
-      GPIO.output(PIN_TRIGGER, GPIO.LOW)
+    GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
-      print("Waiting for sensor to settle")
+    print("Waiting for sensor to settle")
 
-      time.sleep(2)
+    time.sleep(1)
 
-      print("Calculating distance")
+    print("Calculating distance")
 
-      GPIO.output(PIN_TRIGGER, GPIO.HIGH)
+    GPIO.output(PIN_TRIGGER, GPIO.HIGH)
 
-      time.sleep(0.00001)
+    time.sleep(0.00001)
 
-      GPIO.output(PIN_TRIGGER, GPIO.LOW)
+    GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
-      while GPIO.input(PIN_ECHO)==0:
-            pulse_start_time = time.time()
-      while GPIO.input(PIN_ECHO)==1:
-            pulse_end_time = time.time()
+    while GPIO.input(PIN_ECHO)==0:
+        pulse_start_time = time.time()
+    while GPIO.input(PIN_ECHO)==1:
+        pulse_end_time = time.time()
 
-      pulse_duration = pulse_end_time - pulse_start_time
-      distance = round(pulse_duration * 17150, 2)
-      print("Distance:",distance,"cm")
+    pulse_duration = pulse_end_time - pulse_start_time
+    distance = round(pulse_duration * 17150, 2)
+    print("Distance:",distance,"cm")
 
-
+    data = {
+        u'distance': distance
+    }
+    db.collection(u'devices').document(u'DistanceSensor').set(data)
+    time.sleep(5)
 GPIO.cleanup()
-'''
+
+
